3148fab039171a3000cfad60f4699717960d968e
jonoreilly <jonoreilly007@gmail.com>
2017-11-24 11:34:27 +0100

    back to normal

--- js/menu.js
+++ js/menu.js
@@ -34,40 +34,41 @@ function fRecibir (aNumerosAux)
   }
 }
 
+
 function fMenu (aNumerosAux)
 {
   var bExitAux = false;
   while (!bExitAux)
   {
-    var iSeleccion = Number(prompt("Que quieres hacer? \n\n"+
+    var iSeleccion = prompt("Que quieres hacer? \n\n"+
                             "  1. Mostrar el Mayor \n"+
                             "  2. Mostrar la suma de todos los numeros \n"+
                             "  3. Mostrar la media \n"+
                             "  4. Mostrar la suma de los pares y la de los impares \n"+
                             "  5. Mostrar el numero de pares y el de impares \n"+
-                            "  6. Salir"))
+                            "  6. Salir")
     var aAux = new Array();
     switch (iSeleccion)
     {
-      case 1:
+      case "1":
         alert("El mayor es :  "+fMayor(aNumerosAux));
         break;
-      case 2:
+      case "2":
         alert("La suma total es :  "+fSuma(aNumerosAux));
         break;
-      case 3:
+      case "3":
         alert("La media es :  "+fMedia(aNumerosAux));
         break;
-      case 4:
+      case "4":
         aAux = fSumaParesImpares(aNumerosAux);
-        alert("Suma de los pares :  "+aAux[0]+"\nSuma de los impares :  "+aAux[1]);
+        alert(" Suma de los pares :  "+aAux[0]+"\nSuma de los impares :  "+aAux[1]);
         break;
-      case 5:
+      case "5":
         aAux = fContarParesImpares(aNumerosAux)
-        alert("Numero de pares :  "+aAux[0]+"\nNumero de impares :  "+aAux[1]);
+        alert(" Numero de pares :  "+aAux[0]+"\nNumero de impares :  "+aAux[1]);
         break;
-      case 6:
-        alert("aio");
+      case "6":
+        alert("Aio");
         bExitAux = true;
         break;
       default:
@@ -76,10 +77,11 @@ function fMenu (aNumerosAux)
   }
 }
 
+
 function fMayor (aNumerosAuxAux)
 {
-  var iMayorAux = 0;
-  for (var i = 0; i < aNumerosAuxAux.length; i++)
+  var iMayorAux = aNumerosAuxAux[0];
+  for (var i = 1; i < aNumerosAuxAux.length; i++)
   {
     if (aNumerosAuxAux[i] > iMayorAux)
     {
@@ -89,20 +91,23 @@ function fMayor (aNumerosAuxAux)
   return iMayorAux;
 }
 
+
 function fSuma (aNumerosAuxAux)
 {
   var iSumaAux = 0;
   for (var i = 0; i < aNumerosAuxAux.length; i++)
   {
-    iSumaAux = iSumaAux + aNumerosAuxAux[i];
+    iSumaAux += aNumerosAuxAux[i];
   }
   return iSumaAux;
 }
+
 
 function fMedia (aNumerosAuxAux)
 {
   return (fSuma(aNumerosAuxAux)/aNumerosAuxAux.length);
 }
+
 
 function fSumaParesImpares (aNumerosAuxAux)
 {
@@ -112,15 +117,16 @@ function fSumaParesImpares (aNumerosAuxAux)
   {
     if (aNumerosAuxAux[i]%2)
     {
-      iSumaImpares = iSumaImpares + aNumerosAuxAux[i];
+      iSumaImpares += aNumerosAuxAux[i];
     }
     else
     {
-      iSumaPares = iSumaPares + aNumerosAuxAux[i];
+      iSumaPares += aNumerosAuxAux[i];
     }
   }
   return [iSumaPares,iSumaImpares];
 }
+
 
 function fContarParesImpares (aNumerosAuxAux)
 {
