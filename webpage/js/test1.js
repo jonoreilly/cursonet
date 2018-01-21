0dcb8785a183aa75125ee109cdebbb2109a860ad
jonoreilly <jonoreilly007@gmail.com>
2018-01-21 20:01:32 +0100

    miproyecto

--- webpage/js/test1.js
+++ webpage/js/test1.js
@@ -11,3 +11,12 @@ aTexto = prompt ("quiero DAAATOOOSS !!!!");
 alert (aTexto);
 
 document.write (aTexto);
+
+document.addEventListener('keyup', (event) => {
+if( event.key == "ArrowUp")
+{
+  alert ("done");
+}
+else{
+alert('keydown event\n\n' + 'key: ' + event.key);}
+});
